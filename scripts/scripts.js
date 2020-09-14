d001e7bfc2cb1038dac729679c1569d5586a216f
Ernxst <ernest.nkansah-badu.19@ucl.ac.uk>
2020-09-14 20:57:54 +0100

    Fixed scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,21 @@
 function setActiveBtn() {
-    let splitURL = window.location.href.split("/")
-    let activePage = splitURL[splitURL.length - 1];
+    let url = window.location.href
     const header = document.getElementById("nav");
+    let activeBtn = header.getElementsByClassName("active-item")[0];
+    if (activeBtn) {
+        activeBtn.className = "nav-item";
+    }
     const btns = header.getElementsByClassName("nav-item");
     for (let btn of btns) {
-        btn.className = "nav-item"
+        let className;
+        if (url.includes(btn.id)) {
+            className = "active-item";
+        }
+        else {
+            className = "nav-item"
+        }
+        btn.className = className
     }
-    document.getElementById(activePage).className = "active-item";
 }
 
 window.onload = function () {

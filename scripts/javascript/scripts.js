b9b7e81dc300d13d3c63f0968c4cef05ac6ea637
Ernxst <ernest.nkansah-badu.19@ucl.ac.uk>
2020-09-17 19:55:21 +0100

    Reduced navbar icon size for mobile devices
    Attempting to fix navbar link positioning
    
    Attempting to fix navbar icon highlighting

--- scripts/javascript/scripts.js
+++ scripts/javascript/scripts.js
@@ -1,27 +1,27 @@
-function setActiveBtn() {
-    const header = document.getElementById("nav");
+window.onload = function () {
+    let url = window.location.href.toString()
+    const header = document.getElementById("nav-buttons");
+
     let activeBtn = header.getElementsByClassName("active-item")[0];
-    if (activeBtn) {
+    if (activeBtn && ! url.includes(activeBtn.id)) {
         activeBtn.className = "nav-item";
     }
-    let url = window.location.href
-    const btns = header.getElementsByClassName("nav-item");
-    for (let btn of btns) {
-        let className;
-        if (url.includes(btn.id)) {
-            className = "active-item";
+
+    let btns = header.getElementsByClassName("nav-item");
+    let activated = false
+
+    for (let button of btns) {
+        if (url.includes(button.id.toString())) {
+            button.classList.add("active-item")
+            activated = true
         }
         else {
-            className = "nav-item"
+            button.classList.remove("active-item")
         }
-        btn.className = className
     }
-    if (! (url.includes("html"))) {
+
+    if (! activated) {
         let btn = document.getElementById("index.html");
         btn.className = "active-item"
     }
-}
-
-window.onload = function () {
-    setActiveBtn();
 };
